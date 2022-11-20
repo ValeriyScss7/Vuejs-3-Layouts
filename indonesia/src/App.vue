6ef0170e278b721567ab70d0c7bba7370c6e4fb7
ValeryScss7 <lordhtml@mail.ru>
2022-11-20 09:48:15 +0400

    add indonesia page

---
+++ indonesia/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <IndonesiaPage />
+  </div>
+</template>
+
+<script>
+  import IndonesiaPage from '@/components/indonesiaPage.vue'
+
+export default {
+  name: 'App',
+  components:{
+    IndonesiaPage,
+  }
+}
+</script>
+
+<style>
+  @import url(./assets/styles/Style.css);
+  @import url(./assets/styles/IndonesiaMedia.css);
+</style>
---
+++ indonesia/src/components/indonesiaPage.vue
@@ -0,0 +1,207 @@
+<template>
+    <header>
+        <div class="container header__wrap">
+            <div class="header__wrap__top">
+
+                <div class="header__wrap__top__startText">
+                    <span>QWERy</span>
+                </div>
+                
+
+                <div class="header__wrap__top__centerText">
+                    <span>Information</span>
+                    <span>About us</span>
+                    <span>Search</span>
+                </div>
+                
+                <div class="header__wrap__top__endText">
+
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill-opacity="0.5" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.564 14.3392L8.80149 9.57585C6.68288 11.0821 3.76352 10.7136 2.08555 8.72829C0.407578 6.74294 0.530787 3.80301 2.36899 1.96502C4.2067 0.126223 7.14693 0.00248128 9.13264 1.68036C11.1184 3.35825 11.487 6.27789 9.98065 8.39668L14.7432 13.16L13.5648 14.3384L13.564 14.3392ZM5.90399 2.16667C4.32374 2.16632 2.9604 3.27558 2.63938 4.82288C2.31837 6.37018 3.12791 7.93024 4.57788 8.55855C6.02786 9.18685 7.71974 8.71071 8.62919 7.41839C9.53864 6.12608 9.41571 4.37278 8.33482 3.22002L8.83899 3.72002L8.27065 3.15335L8.26065 3.14335C7.63714 2.516 6.78848 2.16429 5.90399 2.16667Z" fill="white"/>
+                        </svg>
+
+                    <span>Search</span>
+                </div>
+                
+            </div>
+
+            <div class="header__wrap__center">
+                <div class="header__wrap__center__heading">
+                    <div class="header__wrap__center__heading__line"></div>
+                    <span>explore the Nature Beauty</span>
+                </div>
+
+                <div class="header__wrap__center__mainText">
+                    <h1>Discover</h1>
+                    <p>Wonderful Indonesia</p>
+
+                </div>
+
+                <div class="header__wrap__center__leftIcons">
+                    <div class="header__wrap__center__leftIcons__wrapper">
+                        <span>Follow us</span>
+                        <img src="../assets/img/instagram.svg" alt="img">
+                        <img src="../assets/img/twitter.svg" alt="img">
+                        <img src="../assets/img/Facebook.svg" alt="img">
+                    </div>
+                    
+                </div>
+
+                <div class="header__wrap__center__bottom">
+                    <span>Scroll down</span>
+                    <img src="../assets/img/arrow_downward_24px.svg" alt="img">
+                </div>
+            </div>
+
+
+        </div>
+    </header>
+
+    <main>
+        <section class="indonesiaTravel">
+            <div class="container indonesiaTravel__wrap">
+
+                <div class="indonesiaTravel__wrap__top">
+                    <div class="indonesiaTravel__wrap__top__left">
+                        <img src="../assets/img/TOpleftImg.png" alt="img">
+                    </div>
+
+                    <div class="indonesiaTravel__wrap__top__right">
+                        <div class="indonesiaTravel__wrap__top__right__heading">
+                            <div class="line"></div>
+                            <span>EAST nUSA TENGGARA</span>
+                        </div>
+
+                        <div class="indonesiaTravel__wrap__top__right__content">
+                            <h3>Have you enjoyed your holiday?</h3>
+                            <p>You will be amazed if you take part in this sailing Komodo island tour package. So it is also mandatory for you, besides enjoying Komodo tourism on Komodo Island, you also have to taste the marine tourism.  The beautiful waters of Komodo will make you meet many travelers from other countries.</p>
+                        </div>
+
+                        <div class="indonesiaTravel__wrap__top__right__link">
+                            <span>read more</span>
+                            <img src="../assets/img/mainarrowbottom.svg" alt="">
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="indonesiaTravel__wrap__center">
+
+                    <div class="indonesiaTravel__wrap__center__heading">
+                        <div class="lineLeft"></div>
+                        <span>Bromo</span>
+                        <div class="lineRight"></div>
+                    </div>
+
+                    <div class="indonesiaTravel__wrap__center__text">
+                        <h3>Steady your steps, we will climb together!</h3>
+                        <p></p>
+                    </div>
+
+                    <div class="indonesiaTravel__wrap__center__bottomLink">
+                        <span>read more</span>
+                        <img src="../assets/img/mainarrowbottom.svg" alt="">
+                    </div>
+
+                    <div class="indonesiaTravel__wrap__center__absoluteButtons">
+                        <img src="../assets/img/leftabsolutebtn.svg" alt="">
+
+                        <img src="../assets/img/rightabsolutebtn.svg" alt="">
+                    </div>
+                </div>
+
+                <div class="indonesiaTravel__wrap__bottom">
+
+                    <div class="indonesiaTravel__wrap__bottom__left">
+                        <div class="indonesiaTravel__wrap__bottom__left__heading">
+                            <div class="line"></div>
+                            <span>INDONESIAN CULTURE</span>
+                        </div>
+
+                        <div class="indonesiaTravel__wrap__bottom__left__text">
+                            <h3>Our culture here is very friendly to people</h3>
+                            <p>known for his politeness, manners and gentleness. This becomes a characteristic when they mingle with other tribes and become basic traits that are passed down by their ancestors.</p>
+                        </div>
+
+                        <div class="indonesiaTravel__wrap__bottom__left__linkBottom">
+                            <span>read more</span>
+                            <img src="../assets/img/mainarrowbottom.svg" alt="img">
+                        </div>
+
+                    </div>
+
+                    <div class="indonesiaTravel__wrap__bottom__right">
+                        <img src="../assets/img/BottomRightimg.png" alt="">
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="container footer__wrap">
+
+            <div class="footer__wrap__top">
+                <div class="footer__wrap__top__left">
+                    <h3>QWERy</h3>
+                </div>
+
+                <div class="footer__wrap__top__right">
+                    <span>Ready to explore?</span>
+                    <a href="#">Get started</a>
+                </div>
+            </div>
+
+            <div class="footer__wrap__main">
+                <div class="footer__wrap__main__content">
+                    <h3>Let's go on vacation, Make your day</h3>
+                    <form action="#">
+                        <input type="email" placeholder="Email address">
+                        <a href="#">
+                            <img src="../assets/img/Footer__link.png" alt="">
+                        </a>
+                    </form>
+                </div>
+                <div class="footer__wrap__main__content">
+                    <h3>Services</h3>
+                    <span>Email Marketing</span>
+                    <span>Campaigns</span>
+                    <span>Branding</span>
+                    <span>Offline</span>
+                </div>
+                <div class="footer__wrap__main__content">
+                    <h3>About</h3>
+                    <span>Our Story</span>
+                    <span>Benefits</span>
+                    <span>Team</span>
+                    <span>Careers</span>
+                </div>
+                <div class="footer__wrap__main__content">
+                    <h3>Help</h3>
+                    <span>FAQs</span>
+                    <span>Contact Us</span>
+                    
+                </div>
+            </div>
+
+            <div class="footer__wrap__bottom">
+                <div class="footer__wrap__bottom__left">
+                    <span>Terms & Conditions</span>
+                    <span>Privacy Policy</span>
+                </div>
+                <div class="footer__wrap__bottom__right">
+                    <img src="../assets/img/Facebook.svg" alt="">
+                    <img src="../assets/img/twitter.svg" alt="">
+                    <img src="../assets/img/instagram.svg" alt="">
+                </div>
+            </div>
+
+            <div class="footer__wrap__copyright">
+                <span>Copyright 2021 QWERy</span>
+            </div>
+        </div>
+    </footer>
+</template>
